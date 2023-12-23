05a281681574bb0b1de3a23f3c25c7a0d59a3b9b
marcelo-mlv <marceloloiolaveras@gmail.com>
2023-12-22 22:49:21 -0300

    add: merging
    now the move_left function consists of sliding the tiles to the left and merging afterwards. Some changes were made to the game interface as well.

--- 2048/boardclass.py
+++ 2048/boardclass.py
@@ -20,28 +20,38 @@ class Board:
         self.grid = []
 
     def print_board(self):
-        print('___________')
         for row in range(self.size):
-            print('| ', end='')
+            print('[ ', end='')
             for col in range(self.size):
                 if self.grid[row][col] == 0:
                     tile = ' '
                 else:
                     tile = self.grid[row][col]
                 print(tile, end=' ')
-            print('|')
-        print('‾‾‾‾‾‾‾‾‾‾‾')
+                if col != self.size-1:
+                    print('-', end=' ')
+            print(']')
+        print()
 
     def start_board(self):
         print("[ New game ]\n")
         self.grid = [[0 for _ in range(self.size)] for _ in range(self.size)]
 
-    def move_left(self):
+    def merge(self):
+        for row in range(self.size):
+            for col in range(self.size - 1):
+                tile = self.grid[row][col]
+                next_tile = self.grid[row][col + 1]
+                if tile == next_tile:
+                    self.grid[row][col] *= 2
+                    self.grid[row][col+1] = 0
+
+    def slide_left(self):
         """
-        Since we're interested on sliding all non-zero tiles to the left, this algorithm
-        consists in swapping two adjacent tiles when the leftmost is zero and the other
-        is different from zero, checking this condition from left to right, and going back
-        to the start while there's still changes to be made. It's kinda like bubblesort, actually
+            Since we're interested on sliding all non-zero tiles to the left, this algorithm
+            consists in swapping two adjacent tiles when the leftmost is zero and the other
+            is different from zero, checking this condition from left to right, and going back
+            to the start while there's still changes to be made. It's kinda like bubblesort, actually
         """
         for row in range(self.size):
             changed = True
@@ -49,21 +59,25 @@ class Board:
                 changed = False
                 for col in range(self.size - 1):
                     tile = self.grid[row][col]
-                    next_tile = self.grid[row][col+1]
+                    next_tile = self.grid[row][col + 1]
                     if tile == 0 and next_tile != 0:
                         changed = True
                         temp = tile
                         self.grid[row][col] = next_tile
-                        self.grid[row][col+1] = temp
+                        self.grid[row][col + 1] = temp
+
+    def move_left(self):
+        self.slide_left()
+        self.merge()
 
     def move_tiles(self, char):
         """
-        Moving Logic: It's easier to program the moving logic to one side only, in this case, left,
-        and use some matrix operations (transpose and mirror vertically) in order to move
-        to other directions while using the same move-to-the-left code.
+            Moving Logic: It's easier to program the moving logic to one side only, in this case, left,
+            and use some matrix operations (transpose and mirror vertically) in order to move
+            to other directions while using the same move-to-the-left code.
 
-        For instance, to move to the right, you can mirror the grid matrix
-        vertically, use the move-left code, and mirror it back!
+            For instance, to move to the right, you can mirror the grid matrix
+            vertically, use the move-left code, and mirror it back!
         """
         if char == 'a':
             self.move_left()
