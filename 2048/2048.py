bb305a83037d3363955dce2a409c08e1380d0d1c
marcelo-mlv <marceloloiolaveras@gmail.com>
2023-12-24 01:44:04 -0300

    add: win dialogue/endless mode
    Now, it's possible to win at this game... and keep going, if requested. It adds a simple text in case of victory and allows the player to keep playing endlessly, with the help of the function play_game, that handles the board class calls.

--- 2048/2048.py
+++ 2048/2048.py
@@ -1,4 +1,5 @@
 import boardclass
+import time
 import os
 
 
@@ -13,6 +14,27 @@ def get_y_n():
     return char
 
 
+def play_game(board, status, endless):
+    """
+    0: Continue
+    1: Loss
+    2: Victory
+    Endless mode: Keep on playing after victory
+
+    This function calls new turns until its status variable equals 1 (referring to loss) or
+    equals 2 (referring to victory, in case it isn't endless already), returning it afterwards.
+    It is explained further in the boardclass file.
+    """
+    game_state = 0
+    if endless:
+        game_state = 2
+    while status == game_state:
+        status = board.new_turn(endless)
+        if status == game_state:
+            os.system("cls")
+    return status
+
+
 print("This is a project based on the game 2048.\n")
 print("Use w, a, s, d to move the tiles accordingly.\n")
 os.system('pause')
@@ -24,11 +46,25 @@ while True:
     os.system("cls")
     print("Press w, a, s or d and then enter! Das it, probably\n\n")
     game.start_board()
-    lose = False
-    while not lose:
-        lose = game.new_turn()
-        if not lose:
-            os.system("cls")
+
+    condition = play_game(game, 0, False)
+
+    if condition == 2:
+
+        print("[ WHAT?? You win? ]")
+        time.sleep(2)
+        print("[ You actually did it! I'm so proud of you :D ]")
+        time.sleep(1)
+        print("[ Would you like to continue? Y/N ]")
+
+        ans = get_y_n()
+        if ans in ['y', 'Y']:
+            os.system('cls')
+            print('[ Endless Mode ]\n')
+            play_game(game, 2, True)
+        else:
+            break
+
     print("[ Game over :( ]\n")
     time.sleep(1)
     print("[ Play again? Y/N ]\n")
