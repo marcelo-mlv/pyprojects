e27aeac493339ac7c5381a010d13b446ab98e9f5
marcelo-mlv <marceloloiolaveras@gmail.com>
2024-01-23 22:49:37 -0300

    add: turn counter, new_turn method

--- chess/chess.py
+++ chess/chess.py
@@ -11,16 +11,18 @@ class Board:
         self.fen = fen
         self.ranks = '8 7 6 5 4 3 2 1'.split()
         self.files = 'a b c d e f g h'
+        self.dim = 8
+        self.turn = 0
 
     def read_fenstring(self):
         positions = self.fen.split('/')
-        
+
         others = positions[-1].split(' ')
         positions.remove(positions[-1])
-        
+
         positions.append(others[0])
         others.remove(others[0])
-        
+
         # print(self.fen)
         # print('POSITIONS:', positions)
         # print('OTHERS:', others)
@@ -37,9 +39,21 @@ class Board:
                 fileindex += 1
 
     def print_board(self):
-        for i in range(8):
+        for i in range(self.dim):
             print(self.ranks[i], end=' ')
-            for j in range(8):
+            for j in range(self.dim):
                 print(self.board[i][j], end=' ')
             print()
         print(' ', self.files)
+        print()
+
+    def get_color_turn(self):
+        if self.turn % 2 == 0:
+            return 'White'
+        return 'Black'
+
+    def new_turn(self):
+        color_turn = self.get_color_turn()
+        print('[ {} move ]\n'.format(color_turn))
+        self.print_board()
+        self.turn += 1
--- chess/game.py
+++ chess/game.py
@@ -1,11 +1,13 @@
 import chess
 import os
 
-FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w"
+FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 test = chess.Board(FEN)
 
 test.read_fenstring()
-test.print_board()
-print('\n')
-os.system('pause')
+
+for _ in range(3):
+    test.new_turn()
+    os.system('pause')
+    os.system('cls')
